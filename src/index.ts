057694f47a0caaa31315aafc4af4b661b5923a9c
sibi <sibivel979@gmail.com>
2021-09-12 21:59:59 -0400

    add canvas element

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-window.addEventListener("load", () => {
-  const header = document.createElement("h1");
-  header.innerText = "Webpack❤️TS"
+window.addEventListener('load', () => {
+  const header = document.createElement('h1');
+  header.innerText = 'Webpack❤️TS Header'
 
-  const body = document.querySelector("body");
-  body.appendChild(header);
+  const body = document.querySelector('body');
+  body.prepend(header);
+
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  ctx.fillStyle = 'cyan'
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 })
